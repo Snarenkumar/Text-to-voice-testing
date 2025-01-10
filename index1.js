cd635b54fa37b54462cf17a224ceba5c9f70a98b
S Naren Kumar <vssnaren@gmail.com>
2025-01-10 23:21:33 +0530

    working with the index1,js file from the server side  0

--- index1.js
+++ index1.js
@@ -66,7 +66,7 @@ app.post("/generate-audio", async (req, res) => {
     }
 });
 
-// ssdfdfRoot edsfndfgaspoint
+// ssdfdfRoot edsfndfgaspointdsf
 app.get("/", (req, res) => {
     res.send("Welcome to the Text-to-Speech API. Use the POST /generate-audio endpoint to generate audio.");
 });
