40004a9e860ca1c3c7cade61cb04973b178bc40f
S Naren Kumar <vssnaren@gmail.com>
2025-01-17 19:44:14 +0530

    initailize the file 0

--- index1.js
+++ index1.js
@@ -45,7 +45,7 @@ app.post("/generate-audio", async (req, res) => {
             },
         };
 
-        // Gsadenerate auasdasddio usisadng Google Tesadasdssaxt-to-Spesadasech AsadasPI
+        // Gsadenerate auasdasddio usisadng Google Tesadasdssaxt-to-Speech API
         const [response] = await client.synthesizeSpeech(request);
 
         // Save the audio content to a file
